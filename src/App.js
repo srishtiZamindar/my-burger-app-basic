adb88c661e4cd97c29c6356922bde2cc482bd63d
Srishti Zamindar <srishtizamindar@gmail.com>
2019-09-18 17:06:47 +0530

    Burger Builder container, dynamic ingredient, proptype validation

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Layout from '../src/components/Layout/Layout';
+import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
       <div >
         <Layout>
-          <p>Test</p>
+          <BurgerBuilder />
         </Layout>
       </div>
     );
